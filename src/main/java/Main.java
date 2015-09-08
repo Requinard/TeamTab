9b314950f890aae1fd339bd94ddde6c532a9285e
Requinard <d.diks@mail.blastedsolutions.com>
2015-09-08 15:33:10 +0200

    Updated diagrams

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Created by david on 1-9-15.
+ */
+public class Main {
+
+	/**
+	 *
+	 * @param args Handles project initialization. Add hooks here!
+	 */
+	public static void main(String[] args) {
+		// TODO - implement Main.main
+		System.out.println("test'");
+	}
+
+	/**
+	 *
+	 * @param args
+	 */
+	private static void initializeMainForm(String[] args) {
+		// TODO - implement Main.initializeMainForm
+		throw new UnsupportedOperationException();
+	}
+
+}
