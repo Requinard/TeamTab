1a023c2fe37fee4bb29e2198602e3efc3937392b
requinard <david.diks@student.fontys.nl>
2015-10-11 17:20:00 +0200

    misc changes

--- src/main/java/gui/MainController.java
+++ src/main/java/gui/MainController.java
@@ -1,7 +1,34 @@
 package gui;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by david on 6-10-15.
  */
-public class MainController {
+public class MainController implements Initializable {
+    @FXML
+    private Button buttonJoin;
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    public void initialize(URL location, ResourceBundle resources) {
+        buttonJoin.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                System.out.println("test");
+            }
+        });
+    }
 }
