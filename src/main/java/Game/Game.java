645669c9ffd27accc40345710a8557987c27219b
Skaj7 <kaj753@hotmail.com>
2015-10-12 11:05:50 +0200

    methodes

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -7,7 +7,31 @@ import java.util.ArrayList;
  */
 public class Game {
     private ArrayList<Team> teams;
+    private ArrayList<Player> players;
+    private ArrayList<Instruction> instructions;
+    private ArrayList<Panel> panels;
+    private int timeRound;
+
     public Game(){
         teams = new ArrayList<Team>();
+        players = new ArrayList<Player>();
+        instructions = new ArrayList<Instruction>();
+        panels = new ArrayList<Panel>();
+    }
+
+    public void startGame(){
+
+    }
+
+    public void newRound(){
+
+    }
+
+    public void endGame(){
+
+    }
+
+    public void addPlayerToTeam(){
+
     }
 }
--- src/main/java/Game/Team.java
+++ src/main/java/Game/Team.java
@@ -1,7 +1,47 @@
 package Game;
 
+import java.util.ArrayList;
+
 /**
  * Created by HP user on 12-10-2015.
  */
 public class Team {
+    private ArrayList<Player> players;
+
+    private int time = 9;
+    private int lives;
+    private int correctInstruction;
+
+    public Team(){
+        players = new ArrayList<Player>();
+    }
+
+    public void addTime(){
+
+    }
+
+    public void addLives(){
+
+    }
+    public void subtractLives(){
+
+    }
+    public void subtractTime(){
+
+    }
+    public void addPlayer(){
+
+    }
+    public void removePlayer(){
+
+    }
+    public void addCorrectInstruction(){
+
+    }
+    public void reset(){
+
+    }
+    public void getPlayerPanels(){
+        
+    }
 }
