6fc0b86059275d2dcad48cb814b0f625bb39d730
requinard <david.diks@student.fontys.nl>
2015-10-12 13:18:23 +0200

    Added test stubs

---
+++ src/test/java/Game/GameTest.java
@@ -0,0 +1,43 @@
+package Game;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by david on 12-10-15.
+ */
+public class GameTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testStartGame() throws Exception {
+
+    }
+
+    @Test
+    public void testNewRound() throws Exception {
+
+    }
+
+    @Test
+    public void testEndGame() throws Exception {
+
+    }
+
+    @Test
+    public void testAddPlayerToTeam() throws Exception {
+
+    }
+}
