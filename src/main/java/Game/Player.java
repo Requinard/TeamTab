3f79bef94798914eab17f02952866b9c0bfa13e2
Qunfong <Qunfong@live.nl>
2015-10-12 11:22:06 +0100

    Update player class

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -1,6 +1,7 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Qun on 12-10-2015.
@@ -10,8 +11,20 @@ public class Player {
     String name;
     int score;
     ArrayList<Panel> panels;
+    ArrayList<Instruction> instructions;
+
 
     public Player() {
         panels = new ArrayList<Panel>();
+        instructions = new ArrayList<Instruction>();
     }
+
+    public void getPanel() {
+        //random panels getten uit de lijst
+        Random r = new Random();
+        int random = r.nextInt(panels.size());
+        panels.get(random);
+
+    }
+
 }
