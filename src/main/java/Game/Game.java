c352cd15676a55b2807f9c9f8396564927e866fb
Skaj7 <kaj753@hotmail.com>
2015-10-12 10:55:52 +0200

    team

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,7 +1,13 @@
 package Game;
 
+import java.util.ArrayList;
+
 /**
  * Created by HP user on 12-10-2015.
  */
 public class Game {
+    private ArrayList<Team> teams;
+    public Game(){
+        teams = new ArrayList<Team>();
+    }
 }
---
+++ src/main/java/Game/Team.java
@@ -0,0 +1,7 @@
+package Game;
+
+/**
+ * Created by HP user on 12-10-2015.
+ */
+public class Team {
+}
