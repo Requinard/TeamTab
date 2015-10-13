3d602294114b617af3548292bea104380cd6a38a
Qunfong <Qunfong@live.nl>
2015-10-13 09:12:16 +0100

    AddedPlayerTest

--- src/test/java/Game/PlayerTest.java
+++ src/test/java/Game/PlayerTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -23,6 +26,22 @@ public class PlayerTest {
 
     @Test
     public void testGetPanel() throws Exception {
+        Panel testPanel;
+        ArrayList<Panel> Panels = new ArrayList<Panel>();
+        for (int i = 0; i < 3 ; i++) {
+            Panels.add(new Panel());
+        }
+        Random r = new Random();
+        int Random = r.nextInt(panels.size());
+        testPanel =  Panels.get(Random);
+        for(Panel p : Panels)
+        {
+            if(p == testPanel)
+            {
+                Assert.assertEquals(Random, Panels.indexOf(p));
+            }
+        }
+
 
     }
 }
