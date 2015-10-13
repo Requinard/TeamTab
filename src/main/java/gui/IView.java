4b61573fea34d54474d399b4d419c07329d34114
requinard <david.diks@student.fontys.nl>
2015-10-13 09:52:44 +0200

    Added javadocs for iview

--- src/main/java/gui/IView.java
+++ src/main/java/gui/IView.java
@@ -4,9 +4,23 @@ package gui;
  * Created by david on 6-10-15.
  */
 public interface IView {
+
+    /**
+     * Attempts to load a view's FXML and render it in the scene
+     * @return Success of the operations
+     */
     boolean load();
 
+    /**
+     * Attempts to deload a scene.
+     * @return Whether the operations was successfull
+     */
     boolean deload();
 
+    /**
+     * Attempts to deload this view, and load the next iView
+     * @param nextView Next view that needs to be loaded
+     * @return Success of the operations
+     */
     boolean pass(IView nextView);
 }
