ca442463a62b81fba3a1589f662f4cac28205152
Qunfong <Qunfong@live.nl>
2015-10-13 13:19:26 +0100

    PanelJavaDocx

--- src/main/java/Game/Instruction.java
+++ src/main/java/Game/Instruction.java
@@ -19,15 +19,28 @@ public class Instruction {
     }
 
     /*
-    * Returns the panel which*/
+    * Returns the panel which is linked to the instruction
+    * */
     public Panel getPanel() {
         return panel;
     }
+
+    /*
+    * Returns the commando the player has to execute
+    * */
     public String getCommando() {
         return commando;
     }
+
+    /*
+    * Returns the value of the instruction
+    * it checks if it is true or false
+    * */
     public int getNewValue() {return newValue;}
 
+    /*
+    * Returns a list of instructions for the panel
+    * */
     public ArrayList<Instruction> getInstructions() {
         return instructions;
     }
