c4c0271e998052d5bb822a4018f680a573b27733
Kevin <jetten.kevin@gmail.com>
2015-10-13 09:05:10 +0200

    Panelcontrols gemaakt

--- src/main/java/gui/panel/AbstractPanelControl.java
+++ src/main/java/gui/panel/AbstractPanelControl.java
@@ -1,7 +1,9 @@
 package gui.panel;
 
+import javafx.scene.layout.VBox;
+
 /**
  * Created by Kevin on 12-10-2015.
  */
- public abstract class AbstractPanelControl {
+ public abstract class AbstractPanelControl extends VBox{
 }
