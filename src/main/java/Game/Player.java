9a04a8e04cafba02cace745180f7c345e5fd9e3f
Kamil <cgstyle@gmail.com>
2015-10-14 15:09:40 +0200

    Edit of the Player and Team class

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -6,7 +6,7 @@ import java.util.Random;
 /**
  * Created by Qun on 12-10-2015.
  */
-public class Player {
+public class Player implements Comparable<Player>{
     private String ipAdress;
     private String name;
     private int score;
@@ -27,51 +27,50 @@ public class Player {
     }
 
     /**
-     *
      * @return IpAdress of the user
      */
 
     public String getIpAdress() {
-        return ipAdress;
+        return this.ipAdress;
     }
 
     /**
-    * @Return the name of the player
-    */
-    public String getName(){
-        return name;
-    }
-
-    /**
-    * @Returns the score of the current player
-    */
-    public int getScore(){
-        return score;
-    }
-
-    /**
-     *@return the game the player is assigned to
+     * @Return the name of the player
      */
-    public Game getGame(){
-        return game;
+    public String getName() {
+        return this.name;
     }
 
     /**
-     *
+     * @Returns the score of the current player
+     */
+    public int getScore() {
+        return this.score;
+    }
+
+    /**
+     * @return the game the player is assigned to
+     */
+    public Game getGame() {
+        return this.game;
+    }
+
+    /**
      * @return the instructions for the player
      */
     public Instruction getInstructions() {
-        return instructions;
+        return this.instructions;
     }
 
     public void setInstructions(Instruction instructions) {
         this.instructions = instructions;
     }
+
     /**
      * @Returns the team the player is in
      */
     public Team getTeam() {
-        return team;
+        return this.team;
     }
 
     public void setTeam(Team team) {
@@ -79,14 +78,25 @@ public class Player {
     }
 
     /**
-     *
      * @return the panels for the player
      */
     public ArrayList<Panel> getPanels() {
-        return panels;
+        return this.panels;
     }
 
     public void setPanels(ArrayList<Panel> panels) {
         this.panels = panels;
+    }
+
+
+    /***
+     * this is the implementation of the Comparable interface. Based on the DESCENDING compare
+     * @param p
+     * @return
+     */
+    @Override
+    public int compareTo(Player p) {
+        int compareScore = ((Player)p).getScore();
+        return compareScore - this.score;
     }
 }
--- src/main/java/Game/Team.java
+++ src/main/java/Game/Team.java
@@ -1,11 +1,13 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by HP user on 12-10-2015.
  */
 public class Team {
+
     private ArrayList<Player> players;
     private ArrayList<Panel> playerPanels;
 
@@ -13,23 +15,18 @@ public class Team {
     private int lives;
     private int correctInstruction;
 
-    //get
 
-
-
-
-
-    public Team(){
-        time = 9;
-        lives = 3;
-        correctInstruction = 0;
+    public Team(int time, int lives, int correctInstructions){
+        this.time =time;
+        this.lives = lives;
+        this.correctInstruction =correctInstructions;
         players = new ArrayList<Player>();
         playerPanels = new ArrayList<Panel>();
     }
 
 
     public int getTime() {
-        return time;
+        return this.time;
     }
 
     public void setTime(int time) {
@@ -37,7 +34,7 @@ public class Team {
     }
 
     public int getCorrectInstruction() {
-        return correctInstruction;
+        return this.correctInstruction;
     }
 
     public void setCorrectInstruction(int correctInstruction) {
@@ -45,7 +42,7 @@ public class Team {
     }
 
     public int getLives() {
-        return lives;
+        return this.lives;
     }
 
     public void setLives(int lives) {
@@ -53,7 +50,7 @@ public class Team {
     }
 
     public ArrayList<Panel> getPlayerPanels() {
-        return playerPanels;
+        return this.playerPanels;
     }
 
     public void setPlayerPanels(ArrayList<Panel> playerPanels) {
@@ -61,10 +58,83 @@ public class Team {
     }
 
     public ArrayList<Player> getPlayers() {
-        return players;
+        return this.players;
     }
 
     public void setPlayers(ArrayList<Player> players) {
         this.players = players;
     }
+
+    /***
+     * If the list doesn't contains the player then this player will be added to this ArrayList<Player>
+     * @param player
+     * @return true when player is added to the list
+     */
+    public boolean addPlayerToTeam(Player player){
+        for(Player p : players){
+            if(!p.equals(player)){
+                players.add(player);
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    /***
+     * If this list containt the player then the player will be removed
+     * @param player
+     * @return true when player is removed from the list
+     */
+    public boolean removePlayer(Player player){
+        for(Player p : players){
+            if(p.equals(player)){
+                players.remove(player);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /***
+     * sets all the variables to their default values and clears the Player & playerPanels List
+     * @return true when all the values are checked for their default values and the above mentioned list's are empty
+     */
+    public boolean resetTeam(){
+        time = 9;
+        lives = 3;
+        correctInstruction = 0;
+        players.clear();
+        playerPanels.clear();
+
+        if(time == 9 && lives == 3 && correctInstruction == 0 && players.size() == 0 && playerPanels.size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    /***
+     * calls the comparable method on the Players list and sorts them DESCENDING
+     * @return the sorted list
+     */
+    public ArrayList<Player> sortedPlayerByScore(){
+         Collections.sort(players);
+        return this.players;
+    }
+
+    /***
+     * Checks if the player is already part of the team.
+     * @param player
+     * @return true if the player is in the team
+     */
+    public boolean isPlayerInTeam(Player player){
+
+        for(Player p : players ){
+            if(p.equals(player)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
