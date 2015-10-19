b9d394a5e55e7855cfa2b62df9b5ba44b76fe32d
Qunfong <Qunfong@live.nl>
2015-10-19 13:16:12 +0100

    Add JavaDoc to project. TeamTest
    Add the javadoc for all methods in TeamTest

--- src/test/java/Game/TeamTest.java
+++ src/test/java/Game/TeamTest.java
@@ -34,6 +34,7 @@ public class TeamTest {
         in2 = new Instruction(pan2, "Click off", 1);
         in3 = new Instruction(pan2, "Set to 50", 1);
         pp1 = new ArrayList<Panel>();
+        pp1.add(pan1);
         p1 = new Player("111.11.11.11", "testPersoon", 2, pp1, in1, g, null);
         p2 = new Player("222.22.22.22", "testPersoon2", 3, pp1, in2, g, null);
         p3 = new Player("333.33.33.33", "testPersoon3", 3, pp1, in3, g, null);
@@ -45,21 +46,39 @@ public class TeamTest {
 
     }
 
-
-    @Test
-    public void testAddPlayerToTeam() throws Exception {
+    /**
+     * @Author Qun
+     * Test AddPlayerToTeam()
+     * Tests Add a player to the team
+     * Tests Add a existing teamplayer to team
+     * @throws Exception
+     */
+    @Test public void testAddPlayerToTeam() throws Exception {
         assertEquals("Player isn't added", true, t.addPlayerToTeam(p1));
         t.addPlayerToTeam(p1);
         assertEquals("Player is added to the list ", false, t.addPlayerToTeam(p1));
     }
 
-    @Test
-    public void testRemovePlayer() {
+    /**
+     * @Author Qun
+     * Test RemovePlayer
+     * Test if player is removed
+     * Test if a player who is not in the team is removed
+     * @throws Exception
+     */
+     @Test
+    public void testRemovePlayer() throws Exception {
         t.addPlayerToTeam(p1);
         assertEquals("Player is not removed", true, t.removePlayer(p1));
         assertEquals("Player is removed", false, t.removePlayer(p1));
     }
 
+    /**
+     * @Author Qun
+     * Test ResetTeam
+     * Test if the team is totally reset
+     * Test Time = 9, Lives = 3, CorrectInstructions = 0
+     */
     @Test
     public void testResetTeam() {
         t.resetTeam();
@@ -69,6 +88,15 @@ public class TeamTest {
         assertEquals("Amount of correct instructions have not been reset ", 0, t.getCorrectInstruction());
     }
 
+    /**
+     * @Author Qun
+     * Test SortedPlayerByScore
+     * Adds 3 testperson
+     * 2 persons with the samen score
+     * 1 person with a lower score
+     * Check if the first person with same score is set first
+     * Check if lowest score is set at the last position
+     */
     @Test
     public void testSortedPlayerByScore() {
         t.addPlayerToTeam(p1);  // Score 2
@@ -80,7 +108,13 @@ public class TeamTest {
         assertEquals("Wrong player is third", p1, t.getPlayers().get(2));
     }
 
-
+    /**
+     *@Author Qun
+     * Test IsPlayerInTeam()
+     * Add a player to team to see if it's in the team
+     * And check if someone is in the team when
+     * it's not added to the team
+     */
     @Test
     public void testIsPlayerInTeam() {
         t.addPlayerToTeam(p1);
@@ -88,10 +122,22 @@ public class TeamTest {
         assertEquals("Player is in team", false, t.isPlayerInTeam(p2));
     }
 
+
+    /**
+     * @Author Qun
+     * Test the private void refreshPlayerPanels()
+     * This is used in addPlayerToTeam and removePlayer
+     * Test if this method adds a panel and removes a panel
+     */
     @Test
     public void testRefreshPlayerPanels() {
-        //Internet advices not to test private methodes several times
-        //Have to ask the teacher about this first before testing it
+        g.addPlayerToTeam(p1);
+        g.addPlayerToTeam(p2);
+        int test = g.team1.getPlayerPanels().size();
+        assertEquals("Panel is not added for the new player team1", 1, test);
+        assertEquals("Panel is not added for the new player team2", 1, g.team2.getPlayerPanels().size());
+        g.team1.removePlayer(p1);
+        assertEquals("Panel is not deleted after remove", 0, g.team1.getPlayerPanels().size());
     }
 }
 
