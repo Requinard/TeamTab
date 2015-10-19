7906feae03ceb33de4a8649bf5285e52f6d3eedd
requinard <david.diks@student.fontys.nl>
2015-10-19 11:38:54 +0200

    Changed panel to set the type

--- src/main/java/Game/Panel.java
+++ src/main/java/Game/Panel.java
@@ -5,20 +5,39 @@ package Game;
  */
 public class Panel {
     private int id;
-    private int type;
+    private PanelTypeEnum type;
     private String text;
     private int min;
     private int max;
     private Instruction instruction;
 
+
+    /**
+     * @param id          Panel number id
+     * @param type        Type of panel (Button, horizontal or vertical)
+     * @param text        Text that gives a description
+     * @param min         Minimum value a panel ccan have
+     * @param max         Maximum value a panel can have
+     * @param instruction
+     * @author David
+     */
     public Panel(int id, int type, String text, int min, int max, Instruction instruction) {
         this.id = id;
-        this.type = type;
+        this.type = PanelTypeEnum.values()[type];
         this.text = text;
         this.min = min;
         this.max = max;
         this.instruction = instruction;
     }
+
+    public Panel(int id, PanelTypeEnum type, String text, int min, int max) {
+        this.id = id;
+        this.type = type;
+        this.text = text;
+        this.min = min;
+        this.max = max;
+    }
+
     /**
     * @Returns the id of the Panel
     */
@@ -28,8 +47,8 @@ public class Panel {
 
     /**
     * @Returns the type of the panel
-    */
-    public int getType(){
+     */
+    public PanelTypeEnum getType(){
         return type;
     }
 
