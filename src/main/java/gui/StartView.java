1a86203d3d45138507d9030279bbd648d0240f52
Kevin <jetten.kevin@gmail.com>
2015-11-15 18:03:13 +0100

    Loggers toegevoegd aan alle views

--- src/main/java/gui/StartView.java
+++ src/main/java/gui/StartView.java
@@ -4,15 +4,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javassist.bytecode.stackmap.TypeData;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Vito Corleone on 6-10-2015.
  */
 public class StartView extends AbstractView implements IView {
     private StartViewController startViewController;
+    private static final Logger log = Logger.getLogger(TypeData.ClassName.class.getName());
+
     public StartView(StageController stageController){
         super(stageController);
         startViewController = new StartViewController();
@@ -22,9 +27,11 @@ public class StartView extends AbstractView implements IView {
      * {@inheritDoc}
      */
     public boolean load() {
+        log.log(Level.INFO, "Start loading StartView");
         Stage stage = new Stage();
         stage.setTitle("StartView");
         URL location = this.getClass().getResource("/StartView.fxml");
+        log.log(Level.INFO, "loaded ScoreView from {0}",location.toString());
         FXMLLoader loader = new FXMLLoader(location);
         loader.setController(new StartViewController());
         try {
@@ -32,14 +39,17 @@ public class StartView extends AbstractView implements IView {
             Scene scene = new Scene(myPane);
             stage.setScene(scene);
             passScene(scene);
+            log.log(Level.FINER, "loaded pane in scene and set the scene");
 
         } catch (IOException e) {
             e.printStackTrace();
+            log.log(Level.SEVERE, e.toString(), e);
             return false;
         }
 
         startViewController = loader.getController();
         startViewController.setView(this);
+        log.log(Level.INFO, "Loaded StartView in view");
 
         return true;
     }
