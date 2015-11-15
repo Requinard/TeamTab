cd7216f3ef9c9e94f6e523c9c5601a42999db264
Kevin <jetten.kevin@gmail.com>
2015-11-15 15:46:16 +0100

    Logger toegevoegd aan de gameview

--- src/main/java/gui/GameView.java
+++ src/main/java/gui/GameView.java
@@ -3,15 +3,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javassist.bytecode.stackmap.TypeData;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Vito Corleone on 6-10-2015.
  */
 public class GameView extends AbstractView implements IView {
     private GameController gameController;
+    private static final Logger log = Logger.getLogger(TypeData.ClassName.class.getName());
 
     /**
      * @param stageController Stagecontroller that is controlling the application
@@ -19,15 +23,18 @@ public class GameView extends AbstractView implements IView {
     public GameView(StageController stageController){
         super(stageController);
         gameController = new GameController();
+
     }
 
     /**
      * {@inheritDoc}
      */
     public boolean load() {
+        log.log(Level.INFO, "Start loading gameview");
         Stage stage = new Stage();
         stage.setTitle("GameView");
         URL location = this.getClass().getResource("/GameView.fxml");
+        log.log(Level.INFO, "loaded gameview from {0}",location.toString());
         FXMLLoader loader = new FXMLLoader(location);
         loader.setController(new GameController());
         try {
@@ -35,14 +42,16 @@ public class GameView extends AbstractView implements IView {
             Scene scene = new Scene(myPane);
             stage.setScene(scene);
             passScene(scene);
+            log.log(Level.FINER, "loaded pane in scene and set the scene");
 
         } catch (IOException e) {
             e.printStackTrace();
+            log.log(Level.SEVERE, e.toString(), e);
         }
 
         gameController = loader.getController();
         gameController.setView(this);
-
+        log.log(Level.INFO,"Loaded gameview in view");
         return true;
     }
 
