16f9a60eafa9bde5d0b80db1759754aacf7dba65
Requinard <d.diks@mail.blastedsolutions.com>
2015-11-16 14:14:03 +0100

    Added adapters

---
+++ src/main/java/Game/adapters/GameAdapter.java
@@ -0,0 +1,22 @@
+package Game.adapters;
+
+import Game.*;
+
+public class GameAdapter {
+
+
+
+	public ClientGame toObject() {
+		// TODO - implement GameAdapter.toObject
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param game
+	 */
+	public String ToString(ClientGame game) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/Game/adapters/PlayerAdapter.java
@@ -0,0 +1,25 @@
+package Game.adapters;
+
+import Game.*;
+
+public class PlayerAdapter {
+
+	/**
+	 * 
+	 * @param player
+	 */
+	public String toString(Player player) {
+		// TODO - implement PlayerAdapter.toString
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param input
+	 */
+	public Player toObject(String input) {
+		// TODO - implement PlayerAdapter.toObject
+		throw new UnsupportedOperationException();
+	}
+
+}
