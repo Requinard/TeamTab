62219eca1495c08fe079fdb3420b10e9fa0fb013
Kamil <cgstyle@gmail.com>
2015-11-22 15:38:47 +0100

    TeamTab-122 ClientGame.registerInvalidInstruction is geimplementeerd + added player parameter to IGame.registerInvalidInstruction +  javadoc aanpassingen

--- src/main/java/Game/IGame.java
+++ src/main/java/Game/IGame.java
@@ -15,46 +15,47 @@ public interface IGame {
 	Collection<Panel> getPanels();
 
 	/**
-	 * 
-	 * @param name
+	 *
+	 * @param name the name of the team
 	 */
 	Team createTeam(String name) throws IllegalFormatException;
 
 	/**
-	 * 
-	 * @param username
-	 * @param ip
+	 *
+	 * @param username the username of the player
+	 * @param ip the ip adres of the player
 	 */
 	Player createPlayer(String username, String ip);
 
 	/**
-	 * 
-	 * @param player
-	 * @param team
+	 *
+	 * @param player the player that will be assigned to the team
+	 * @param team the team that will get the player
 	 */
 	void assignTeam(Player player, Team team);
 
 	/**
-	 * 
-	 * @param hard
+	 *
+	 * @param hard if true than hardreset
 	 */
 	void reset(boolean hard);
 
 	ClientGame startRound();
 
 	/**
-	 * 
-	 * @param player
-	 * @param panel
+	 *
+	 * @param player the player of which the panel has been changed
+	 * @param panel the panel that is pressed
 	 */
 	boolean processPanel(Player player, Panel panel);
 
 	boolean hasGameEnded();
 
 	/**
-	 * 
-	 * @param instruction
+	 *
+	 * @param instruction the instruction that was to late
+	 * @param player the player that was to late with the instruction
 	 */
-	boolean registerInvalidInstruction(Instruction instruction);
+	boolean registerInvalidInstruction(Player player, Instruction instruction);
 
 }
