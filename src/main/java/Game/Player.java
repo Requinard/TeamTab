df06ceb4c7a42a63f0e1c9f2d8ab3bcc999e470d
Kamil <cgstyle@gmail.com>
2015-11-22 14:56:05 +0100

    TeamTab-151 Player.generateInstruction genereert nu een instruction van teampanels, panels lijst van teams staat synchroon, kleine javadocs aanpassingen gemaakt

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -20,13 +20,14 @@ public class Player {
     /**
      * Constructor of the player
      * author Frank Hartman
+     *
      * @param username the username of the player
-     * @param ip the ip address of the player
+     * @param ip       the ip address of the player
      */
     public Player(String username, String ip) {
         this.username = username;
         this.ip = ip;
-        panels = new ArrayList<Panel>();
+        panels = new ArrayList<>();
     }
 
     /**
@@ -50,6 +51,7 @@ public class Player {
     /**
      * Get the IP of the player
      * author Frank Hartman
+     *
      * @return the ip of the player
      */
     public String getIp() {
@@ -59,6 +61,7 @@ public class Player {
     /**
      * Get the panels of the player
      * author Frank Hartman
+     *
      * @return the panels of the player
      */
     public List<Panel> getPanels() {
@@ -76,7 +79,8 @@ public class Player {
     /**
      * Get the team of the player
      * author Frank Hartman
-     * @return
+     *
+     * @return the team of the player
      */
     public Team getTeam() {
         return this.team;
@@ -84,6 +88,7 @@ public class Player {
 
     /**
      * Set the team of the player
+     *
      * @param team the team of the player
      */
     public void setTeam(Team team) {
@@ -92,6 +97,7 @@ public class Player {
 
     /**
      * Give the active instruction of the player
+     *
      * @return The instruction of the player
      */
     public Instruction getActiveInstruction() {
@@ -101,6 +107,7 @@ public class Player {
     /**
      * This method generates random panels for a player. A player gets 12 random panels
      * author Qunfong, Kaj
+     *
      * @param teamPanels Panels which are given for a team
      * @return Panels that have been added to a player his panels
      */
@@ -115,10 +122,9 @@ public class Player {
         for (int i = 0; i < PANELSPERPLAYER; i++) {
             //Gets random panel from teamPanels and adds this to the players panel
             Panel randomSelectedPanel = teamPanels.get(random.nextInt(teamPanels.size()));
-            if(!panels.contains(randomSelectedPanel)) {
+            if (!panels.contains(randomSelectedPanel)) {
                 panels.add(randomSelectedPanel);
-            }
-            else{
+            } else {
                 i--;
             }
         }
@@ -126,43 +132,40 @@ public class Player {
         return panels;
     }
 
-    /** Author Kamil Wasylkiewicz
+    /**
+     * Author Kamil Wasylkiewicz
      * Check if the player currently has the exact same panel as parameter
-     * @param panel
-	 */
+     *
+     * @param panel the panel that needs to be checked
+     */
     public boolean hasPanel(Panel panel) {
         return panels.contains(panel);
     }
 
     /**
      * This method wil generate an instruction for this player based on a random panel and random value of the panel
-     * author Kamil Wasylkiewicz
+     * author Kamil Wasylkiewicz     *
      *
-     * @return
+     * @return a new made instruction
      */
     public Instruction generateInstruction() {
         Random random = new Random();
-        try {
-            // Get all the team panels
-            List<Panel> teamPanels = (List<Panel>) team.getPanels();
-            // Get a random panel
-            Panel instructionPanel = teamPanels.get(random.nextInt(panels.size()));
-            // create a random between the min en max value of a panel, however random has no constructor with min and max value
-            // the solution is as follows
-            // (panel.getmaxvalue -  panel.getminvalue) + panel.getminvalue
-            // suppose example panel has minimal 2 and maximal 8 value
-            // random.nextint(8-2)+2
-            // this will always produce a random number from minimal 2 and maximal 8
-            int intendedValue = random.nextInt(instructionPanel.getMaximumValue() - instructionPanel.getMinimumValue()) + instructionPanel.getMinimumValue();
-            // new instruction is made
-            activeInstruction = new Instruction(instructionPanel, intendedValue, this);
-            log.log(Level.INFO, "new instruction has been generated");
+        // Get all the team panels
+        List<Panel> teamPanels = (List<Panel>) team.getPanels();
+        // Get a random panel
+        Panel instructionPanel = teamPanels.get(random.nextInt(panels.size()));
+        // create a random between the min en max value of a panel, however random has no constructor with min and max value
+        // the solution is as follows
+        // (panel.getmaxvalue -  panel.getminvalue) + panel.getminvalue
+        // suppose example panel has minimal 2 and maximal 8 value
+        // random.nextint(8-2)+2
+        // this will always produce a random number from minimal 2 and maximal 8
+        int intendedValue = random.nextInt(instructionPanel.getMaximumValue() - instructionPanel.getMinimumValue()) + instructionPanel.getMinimumValue();
+        // new instruction is made
+        activeInstruction = new Instruction(instructionPanel, intendedValue, this);
+        log.log(Level.INFO, "Instruction " + activeInstruction.getPanel().getText() + " set to " + activeInstruction.getIntendedValue());
 
-            return activeInstruction;
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        return null;
+        return activeInstruction;
     }
 
 }
