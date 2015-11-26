366e6ae354603451fed13f4de81e37ffddca8fc6
Kevin <jetten.kevin@gmail.com>
2015-11-26 18:19:45 +0100

    Aanvulling HostMediator
    Invulling gedaan van de teams en instruction

--- src/main/java/networking/mediator/HostMediator.java
+++ src/main/java/networking/mediator/HostMediator.java
@@ -1,11 +1,17 @@
 package networking.mediator;
 
 import Game.HostGame;
+import Game.Instruction;
 import Game.Player;
+import Game.Team;
+import Game.adapters.InstructionAdapter;
 import Game.adapters.PlayerAdapter;
+import Game.adapters.TeamAdapter;
 import networking.server.NetworkRequest;
 import networking.server.RequestType;
+import sun.nio.ch.Net;
 
+import javax.print.DocFlavor;
 import java.util.List;
 
 /**
@@ -36,7 +42,23 @@ public class HostMediator extends BaseMediator implements IMediator {
             case "/player/":
                 handlePlayers(networkRequest);
                 break;
-
+            case "/instruction/":
+                handleInstruction(networkRequest);
+                break;
+            case "/teams/":
+                handleTeams(networkRequest);
+                break;
+            case "/teams/assign":
+                handleTeamsAssign(networkRequest);
+            case "/teams/create/":
+                handleTeamsCreate(networkRequest);
+                break;
+            case "/panels/":
+                handlePanels(networkRequest);
+                break;
+            case"/status/":
+                handleStatus(networkRequest);
+                break;
         }
     }
     @Override
@@ -52,16 +74,40 @@ public class HostMediator extends BaseMediator implements IMediator {
 
             networkServer.send(response.toString(), networkRequest.getNetworkMessage().getSender());
         }
+
     }
 
     @Override
     public void handleInstruction(NetworkRequest networkRequest) {
+        if(networkRequest.getType() == RequestType.GET){
+            String ip = networkRequest.getNetworkMessage().getSender();
+            Instruction latestInstruction = getPlayer(ip).getActiveInstruction();
 
+            //JSonify instruction
+            String json = InstructionAdapter.toString(latestInstruction);
+
+            NetworkRequest response = new NetworkRequest(RequestType.SEND, networkRequest.getUrl(), json);
+
+            networkServer.send(response.toString(), ip);
+        }
+        if(networkRequest.getType() == RequestType.POST){
+            //todo In de api kijken of het hier om gaat
+            Instruction expiredInstruction = InstructionAdapter.toObject(networkRequest.getPayload());
+            hostGame.registerInvalidInstruction(expiredInstruction);
+        }
     }
 
     @Override
     public void handleTeams(NetworkRequest networkRequest) {
+        if (networkRequest.getType() == RequestType.GET) {
+            //Retrieve teams
+            List<Team> teams = hostGame.getTeams();
 
+            String json = TeamAdapter.toString(teams);
+
+            NetworkRequest response = new NetworkRequest(RequestType.SEND, networkRequest.getUrl(), json);
+            networkServer.send(response.toString(), networkRequest.getNetworkMessage().getSender());
+        }
     }
 
     @Override
@@ -71,6 +117,32 @@ public class HostMediator extends BaseMediator implements IMediator {
 
     @Override
     public void handleStatus(NetworkRequest networkRequest) {
+        if(networkRequest.getType() == RequestType.GET){
+            //todo Deze zal pas later worden geimplementeerd op het moment dat we precies weten welke informatie nodig is
+        }
+    }
+    public void handleTeamsCreate(NetworkRequest networkRequest){
+        Team team = TeamAdapter.toObject(networkRequest.getPayload());
+        hostGame.createTeam(team.getName());
+    }
+    public void handleTeamsAssign(NetworkRequest networkRequest)
+    {
+        Team teamRequest = TeamAdapter.toObject(networkRequest.getPayload());
+        for(Team team : hostGame.getTeams()) {
+            if(team.getName() == teamRequest.getName()) {
+                hostGame.assignTeam(getPlayer(networkRequest.getNetworkMessage().getSender()),team);
+            }
+        }
+    }
 
+    //gets player by IP
+    public Player getPlayer(String ipadress)
+    {
+        Player returnPlayer = null;
+        for(Player player : hostGame.getPlayers()){
+            if(player.getIp().equals(ipadress))
+                returnPlayer = player;
+        }
+        return returnPlayer;
     }
 }
