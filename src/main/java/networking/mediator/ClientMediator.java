cba40fec7a80f5a8ab8baca9135d2b8effc92cbd
Skaj7 <kaj753@hotmail.com>
2015-12-07 23:31:17 +0100

    loggers voor clientmediator toegevoegd

--- src/main/java/networking/mediator/ClientMediator.java
+++ src/main/java/networking/mediator/ClientMediator.java
@@ -7,20 +7,27 @@ import Game.adapters.PlayerAdapter;
 import Game.adapters.TeamAdapter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import javassist.bytecode.stackmap.TypeData;
 import networking.server.NetworkRequest;
 import networking.server.RequestType;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ClientMediator extends BaseMediator implements IMediator {
+    private static final Logger log = Logger.getLogger(TypeData.ClassName.class.getName());
     ClientGame clientGame;
 
     public ClientMediator(ClientGame clientGame, int port) {
         super();
+        log.log(Level.INFO, "client mediator has started");
         this.clientGame = clientGame;
+        log.log(Level.INFO, "client set");
+        log.log(Level.INFO, "client mediator has ended");
     }
 
     public ClientMediator(ClientGame clientGame) {
@@ -31,60 +38,74 @@ public class ClientMediator extends BaseMediator implements IMediator {
     @Override
     public void handlePlayers(NetworkRequest networkRequest) {
         if (networkRequest.getType() == RequestType.SEND) {
+            log.log(Level.FINER, "client handlePlayers has started, networkRequest type is SEND");
             List<Player> players = PlayerAdapter.toObjects(networkRequest.getPayload());
-
+            log.log(Level.FINER, "client players contains: {0} players", players.size());
             clientGame.setPlayers(players);
-
+            log.log(Level.FINER, "client players have been set");
             NetworkRequest getTeamRequest = new NetworkRequest(RequestType.GET, "/teams/players/", null);
 
             networkServer.sendRequest(getTeamRequest, networkRequest.getNetworkMessage().getSender());
+            log.log(Level.FINER, "client handlePlayers has ended, networkRequest has been send");
         } else {
             networkServer.requeueRequest(networkRequest);
+            log.log(Level.FINE, "else statement handlePlayers reached, requeueRequest uitgevoerd");
         }
     }
 
     @Override
     public void handleInstruction(NetworkRequest networkRequest) {
         if (networkRequest.getType() == RequestType.SEND) {
+            log.log(Level.FINER, "client handleInstruction has started, networkRequest type is SEND");
             Instruction instruction = InstructionAdapter.toObject(networkRequest.getPayload());
+            log.log(Level.FINER, "client instruction contains panel text: {0}", instruction.getPanel().getText());
 
             clientGame.localInstruction = instruction;
+            log.log(Level.FINER, "client handleInstruction has ended, instruction has been set");
         }
     }
 
     @Override
     public void handleTeamPlayers(NetworkRequest networkRequest) {
         if (networkRequest.getType() == RequestType.SEND) {
+            log.log(Level.FINER, "client handleTeamPlayers has started, networkRequest type is SEND");
             HashMap<String, List<String>> map;
 
             Type t = new TypeToken<HashMap<String, List<String>>>() {
             }.getType();
 
             map = new Gson().fromJson(networkRequest.getPayload(), t);
-
+            log.log(Level.FINER, "client handleTeamPlayers map team contains: {0} teams", map.size());
             clientGame.setTeams(map);
+            log.log(Level.FINER, "client handleTeamPlayers has ended, teams have been set");
         }
     }
 
     @Override
     public void handleTeams(NetworkRequest networkRequest) {
         if (networkRequest.getType() == RequestType.SEND) {
+            log.log(Level.FINER, "client handleTeams has started, networkRequest type is SEND");
             List<Team> teams = TeamAdapter.toObjects(networkRequest.getPayload());
-
+            log.log(Level.FINER, "client teams contains: {0} teams", teams.size());
             clientGame.setTeams(teams);
+            log.log(Level.FINER, "client handleTeams has ended, teams have been set");
         } else {
             networkServer.requeueRequest(networkRequest);
+            log.log(Level.FINE, "else statement handleTeams reached, requeueRequest uitgevoerd");
         }
     }
 
     @Override
     public void handlePanels(NetworkRequest networkRequest) {
         if (networkRequest.getType() == RequestType.SEND) {
+            log.log(Level.FINER, "client handlePanels has started, networkRequest type is SEND");
             List<Panel> panels = PanelAdapter.toObjects(networkRequest.getPayload());
-
+            log.log(Level.FINER, "client panels contains: {0} teams", panels.size());
             clientGame.setPanels(panels);
+            log.log(Level.FINER, "client handlePanels has ended, panels have been set");
         } else {
             networkServer.requeueRequest(networkRequest);
+            log.log(Level.FINE, "else statement handlePanels reached, requeueRequest uitgevoerd");
         }
     }
 
@@ -104,38 +125,50 @@ public class ClientMediator extends BaseMediator implements IMediator {
     }
 
     public void createPlayer(Player player) {
+        log.log(Level.FINER, "client createPlayer with player name: {0} has started", player.getUsername());
         NetworkRequest request = new NetworkRequest(RequestType.POST, "/players/", PlayerAdapter.toString(player));
 
         networkServer.send(request.toString(), clientGame.getHostIP());
+        log.log(Level.FINER, "client createPlayer has ended, POST NetworkRequest has been send");
     }
 
     public void createTeam(Team team) {
+        log.log(Level.FINER, "client createTeam with team name: {0} has started", team.getName());
         NetworkRequest request = new NetworkRequest(RequestType.POST, "/teams/create/", TeamAdapter.toString(team));
 
         networkServer.send(request.toString(), clientGame.getHostIP());
+        log.log(Level.FINER, "client createTeam has ended, POST NetworkRequest has been send");
     }
 
     public void assignTeam(Team team) {
+        log.log(Level.FINER, "client assignTeam to team name: {0} has started", team.getName());
         NetworkRequest request = new NetworkRequest(RequestType.POST, "/teams/assign/", TeamAdapter.toString(team));
 
         networkServer.send(request.toString(), clientGame.getHostIP());
+        log.log(Level.FINER, "client assignTeam has ended, POST NetworkRequest has been send");
     }
 
     public void processPanel(Panel panel) {
+        log.log(Level.FINER, "client processPanel with panel text: {0} has started", panel.getText());
         NetworkRequest request = new NetworkRequest(RequestType.POST, "/panels/", PanelAdapter.toString(panel));
 
         networkServer.send(request.toString(), clientGame.getHostIP());
+        log.log(Level.FINER, "client processPanel has ended, POST NetworkRequest has been send");
     }
 
     public void registerInvalidInstruction(Instruction instruction) {
+        log.log(Level.FINER, "client registerInvalidInstruction with instruction panel text: {0} has started", instruction.getPanel().getText());
         NetworkRequest request = new NetworkRequest(RequestType.POST, "/instruction/", InstructionAdapter.toString(instruction));
 
         networkServer.send(request.toString(), clientGame.getHostIP());
+        log.log(Level.FINER, "client registerInvalidInstruction has ended, POST NetworkRequest has been send");
     }
 
     public void setPlayerStatus(Player localePlayer) {
+        log.log(Level.FINER, "client setPlayerStatus with player name: {0} has started", localePlayer.getUsername());
         NetworkRequest request = new NetworkRequest(RequestType.POST, "/players/changeStatus/", PlayerAdapter.toString(localePlayer));
 
         networkServer.send(request.toString(), clientGame.getHostIP());
+        log.log(Level.FINER, "client setPlayerStatus has ended, POST NetworkRequest has been send");
     }
 }
