07ad94f68f00d71f301d8a9332ea99dbb7f58f60
David Diks <contact@terarion.com>
2015-12-09 17:13:41 +0100

    Changed finder to work with any network subentter to mask 8

--- src/main/java/networking/finder/GameFinder.java
+++ src/main/java/networking/finder/GameFinder.java
@@ -1,49 +1,49 @@
 package networking.finder;
 
-import com.sun.javafx.UnmodifiableArrayList;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Label;
-import sun.nio.ch.ThreadPool;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observer;
-import java.util.concurrent.Callable;
+import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 /**
  * Created by Kevin on 30-11-2015.
  */
 public class GameFinder {
     ExecutorService pool = Executors.newFixedThreadPool(10);
+    private final int TIMEOUT = 10;
 
     /**
-     *Finds all the hosts which are on the subnet
-     *@param list is the listview which will receive all the ip's of available games
+     * Finds all the hosts which are on the same subnet as the finder
+     * Author: David
+     * WORKS ONLY FOR MASKS OF 8
+     *
+     * @param list is the listview which will receive all the ip's of available games
      */
     public void findGames(ObservableList list) {
         ObservableList openServers = list;
+        String localHost;
+        try {
+            localHost = Inet4Address.getLocalHost().toString();
+        } catch (UnknownHostException e) {
+            localHost = "192.168.0.1";
+        }
 
-        String subnet = "192.168";
-        int timeout = 100;
-        int prefix = 223 ;
-        
-        for (int i = 1; i < 255; i++) {
-            String host = subnet + "." + prefix + "." + i;
+
+        // Prepare for subnetting
+        String[] nibbles = localHost.split("/")[1].split("\\.");
+
+        localHost = String.format("%s.%s.%s",nibbles[0], nibbles[1], nibbles[2]);
+
+        for (int i = 1; i <= 255; i++) {
+            String host = String.format("%s.%s", localHost, i);
             pool.execute(() -> {
                 SocketAddress sockaddr = new InetSocketAddress(host, 8085);
                 Socket socket = new Socket();
                 try {
-                    socket.connect(sockaddr, timeout);
+                    socket.connect(sockaddr, TIMEOUT);
                     Platform.runLater(new Runnable() {
                         @Override
                         public void run() {
@@ -54,13 +54,10 @@ public class GameFinder {
                     //unreachable host
                 }
             });
-            if (prefix < 224 && i == 254) {
-                i = 1;
-                prefix++;
-            }
         }
     }
-    public boolean findSingleGame(String ipAddress){
+
+    public boolean findSingleGame(String ipAddress) {
         boolean connectionValid = false;
         SocketAddress sockaddr = new InetSocketAddress(ipAddress, 8085);
         Socket socket = new Socket();
