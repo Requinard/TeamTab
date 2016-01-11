9150ea397a37fee4ee67d9e77e123b7afd3d0d2b
Skaj7 <kaj753@hotmail.com>
2016-01-11 14:30:36 +0100

    handle fix

--- src/main/java/networking/mediator/BaseMediator.java
+++ src/main/java/networking/mediator/BaseMediator.java
@@ -67,7 +67,6 @@ public abstract class BaseMediator implements IMediator {
         } else if (networkRequest.getUrl().equals("/teams/assign/")) {
             handleTeamsAssign(networkRequest);
 
-            handleTeamsCreate(networkRequest);
         } else if (networkRequest.getUrl().equals("/teams/players/")) {
             handleTeamPlayers(networkRequest);
 
